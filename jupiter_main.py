760c02bc74e02db8f2081a9e867032eefff5bc7b
jacopograssi97 <jacopograssi977@gmail.com>
2023-03-16 09:36:04 +0100

    Add files via upload

---
+++ jupiter_main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+# using the style for the plot
+plt.style.use('seaborn-v0_8-darkgrid')
+
+font = {'family' : 'Arial',
+        'weight' : 'normal',
+        'size'   : 12}
+
+matplotlib.rc('font', **font)
+
+
+st.set_page_config(page_title="time series", page_icon="📈")
+
