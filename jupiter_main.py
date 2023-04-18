b150e284b36034c8b622145c9c8e802376845bec
jacopograssi97 <jacopograssi977@gmail.com>
2023-04-18 14:35:23 +0200

    Add files via upload

--- jupiter_main.py
+++ jupiter_main.py
@@ -15,8 +15,14 @@ font = {'family' : 'Arial',
 matplotlib.rc('font', **font)
 
 
-st.header('Dashboard for Jupiter Intelligence data analysis - v 0.1')
+st.header('Dashboard for Jupiter Intelligence data analysis - v 0.2')
 
-st.subheader('Updates')
+
+st.subheader('Updates - v 0.2')
+st.text('-Bug fixed \n-Section Spatial metrics added')
+
+st.divider()
+
+st.subheader('Updates - v 0.1')
 st.text('-Bug fixed \n-Dashed and dotted lines added for lower and upper limits in Time series \n-Tables simplified in Time series')
 
