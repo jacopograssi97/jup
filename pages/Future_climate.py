432130e7f21381393307d972fd957a9d5f8d0211
jacopograssi97 <jacopograssi977@gmail.com>
2023-04-20 15:18:42 +0200

    Add files via upload

--- pages/Future_climate.py
+++ pages/Future_climate.py
@@ -10,7 +10,7 @@ plt.style.use('seaborn-v0_8-darkgrid')
 
 font = {'family' : 'Arial',
         'weight' : 'normal',
-        'size'   : 12}
+        'size'   : 13}
 
 matplotlib.rc('font', **font)
 
@@ -20,7 +20,7 @@ st.set_page_config(page_title="Future climate", page_icon="📈")
 def save_img(fig):
     fig.savefig('prova.png', dpi=600, bbox_inches='tight')
 
-
+mth = ['Jan','Feb','Mar','Apr','May','Jun','Jul', 'Aug','Sep','Oct','Nov','Dec']
 ###############################################################
 #######################   DATA LOADER #########################
 ###############################################################
@@ -45,10 +45,85 @@ try:
     ###############################################################
     st.subheader('Climatology on monthly mean values')
 
-    st.write('WORK IN PROGRESS')
-    st.write('Soon available')
+    fig,ax = plt.subplots(1,1, figsize=(7,4), dpi=100)
+
+    columns = file.columns.to_list()
+
+    variable = st.radio('Select variable', ['Precipitation', 'Temperature'])
+
+    st.subheader('Seasonal Cycles')
+
+    aggregation = st.radio('Select Aggregation', ['Reference periods', 'Years'])
+
+    file=pd.read_excel(uploaded_file_data, sheet_name='Cycles')
+
+    if variable == 'Precipitation':
+
+        col = [c for c in columns if 'PR' in c]
+        ylab = '[mm/month]'
+
+    if variable == 'Temperature':
+
+        col = [c for c in columns if 'HT' in c]
+        ylab = '[°C]'
+        
+    col.append('years')    
+    file_plot = file[col]
+    variable_to_plot = file_plot.columns.to_list()
+
+
+    scenarios = st.multiselect('Select scenario', ['ssp126','ssp245','ssp585'])
+    bands = st.multiselect('Select band', ['lower', 'mean', 'upper'])
+    colors_585 = ['orange', 'red', 'maroon']
+    colors_126 = ['lime', 'olive', 'darkgreen']
+    colors_245 = ['lightskyblue', 'royalblue', 'navy']
+
+    for scen in scenarios:
+
+        for band in bands:
+
+            for c in col:
+
+                if scen in c and band in c:
+
+                    if scen == 'ssp126':
+                        colors = colors_126
+
+                    if scen == 'ssp585':
+                        colors = colors_585
+
+                    if scen == 'ssp245':
+                        colors = colors_245
+
+                    for periods,color in zip([[2020,2040],[2040,2060],[2080,2100]],colors):
+
+                        file_plotti = file_plot.where(file_plot.years > periods[0])
+                        file_plotti = file_plotti.where(file_plotti.years < periods[1]).dropna()
+                        var = file_plotti[c].to_numpy().reshape((3,12)).mean(axis=0)
+
+                        if band == 'lower':
+                            ax.plot(mth, var, label=f'{periods[0]}-{periods[1]} {scen} {band}', lw=3, color = color, ls=':')
+
+                        if band == 'mean':
+                            ax.plot(mth, var, label=f'{periods[0]}-{periods[1]} {scen} {band}', lw=3, color = color, ls='-')
+
+                        if band == 'upper':
+                            ax.plot(mth, var, label=f'{periods[0]}-{periods[1]} {scen} {band}', lw=3, color = color, ls='--')
+
+    
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), frameon=False)
+
+    title = st.text_input('Chart title', '')
+    ylabel = st.text_input('Y label', ylab)
+
+    ax.set_xlabel('Month')
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+
+    st.pyplot(fig)
 
 
     
 except:
+    raise
     st.write('Load the file')
